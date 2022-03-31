94f76f1a575dd4443909c3f31a2a237f6aa674f7
Grygorii <nakan88@gmail.com>
2022-03-31 16:17:28 +0300

    update client test title

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders app with correct title', () => {
   render(<App />);
   const titleElement = screen.getByText(/TODO list app/i);
   expect(titleElement).toBeInTheDocument();
